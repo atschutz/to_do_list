02296bd3103a95bbacb432990f8b88b143b0d526
atschutz <Aroc4aroc4$>
2021-04-27 14:34:10 -0400

    Added tasks and delete/reminder functionality.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Fight someone',
+        day: 'Tuesday',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'Buy puppy',
+        day: 'Wednesday',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Nail React interview',
+        day: 'Saturday',
+        reminder: false
+    },
+  ])
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id
+      ? { ...task, reminder: !task.reminder } : task)) 
+  }
 
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks to Show'
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
