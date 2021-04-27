26ed6795d7e6465d478412ddbfd59c1443d8f05a
atschutz <Aroc4aroc4$>
2021-04-27 14:59:52 -0400

    Added Add Task functionality.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
-
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -25,6 +26,16 @@ function App() {
     },
   ])
 
+  // Show Add Task
+  // const showAddTask = () 
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
+
   // Delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -38,7 +49,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete=
         {deleteTask} onToggle={toggleReminder} />
